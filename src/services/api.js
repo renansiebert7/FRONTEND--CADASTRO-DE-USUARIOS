07e40b65a0329f0a8ac94617103e790d6e86c2ab
renansiebert7 <renansiebert01@gmail.com>
2026-02-07 23:52:09 -0300

    mudanças api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://node-backend-cadastro-de-usuarios.onrender.com',
 });
 
 export default api;
